eff32d30cee36cb49c787fcab70dbbd417266c90
jrosebr <jrosebr@iu.edu>
2024-02-09 22:21:52 -0500

    updated tests

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -9,15 +9,60 @@ public class BinaryTreeTest {
 
     BinaryTree<Integer> T;
 
-    @BeforeEach
-    public void setUp() throws Exception {
+    @Test
+    public void normal_first()
+    {
 
         Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         ArrayList<Integer> arr_list = new ArrayList<>(List.of(arr));
         T = new BinaryTree<>(arr_list);
 
-        System.out.println(T.getRoot().first());
-        System.out.println(T.getRoot().last());
+        assertEquals(1, T.getRoot().first().data);
+    }
+
+    @Test
+    public void normal_last()
+    {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<Integer> arr_list = new ArrayList<>(List.of(arr));
+        T = new BinaryTree<>(arr_list);
+
+        assertEquals(9, T.getRoot().last().data);
+    }
+
+    @Test
+    public void normal_nextAncestor()
+    {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<Integer> arr_list = new ArrayList<>(List.of(arr));
+        T = new BinaryTree<>(arr_list);
+
+        assertEquals(null, T.getRoot().nextAncestor());
+        assertEquals(null, T.getRoot().right.nextAncestor());
+        assertEquals(5, T.getRoot().left.nextAncestor().data);
+    }
+
+    @Test
+    public void normal_prevAncestor()
+    {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<Integer> arr_list = new ArrayList<>(List.of(arr));
+        T = new BinaryTree<>(arr_list);
+
+        assertEquals(null, T.getRoot().prevAncestor());
+        assertEquals(null, T.getRoot().left.prevAncestor());
+        assertEquals(5, T.getRoot().right.prevAncestor().data);
+    }
+
+    @Test
+    public void normal_next()
+    {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        ArrayList<Integer> arr_list = new ArrayList<>(List.of(arr));
+        T = new BinaryTree<>(arr_list);
+
+        assertEquals("2", T.getRoot().first().next());
+        assertEquals("5", T.getRoot().last().next());
     }
 
     @Test
